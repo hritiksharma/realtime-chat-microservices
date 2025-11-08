5e1447b6fbd52c58ce628242bd6e1e6cf2644020
hritik sharma <hritik2805@gmail.com>
2025-11-08 17:04:27 +0530

    feat: create mail service and add rabbitMQ setup

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -3,10 +3,13 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import { createClient } from 'redis'
 import userRoutes from './routes/user.js';
+import { connectRabbitMQ } from './config/rabbitmq.js';
 
 dotenv.config();
 
  connectDB();
+
+ connectRabbitMQ();
 
 if (!process.env.REDIS_URL) {
   throw new Error('❌ Missing REDIS_URL environment variable');
