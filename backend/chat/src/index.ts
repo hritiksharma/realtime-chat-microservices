527ee398f27a12825508c4b2b2415b3aac85d9ea
hritik sharma <hritik2805@gmail.com>
2025-11-10 12:51:37 +0530

    fix: create new chat route

---
+++ backend/chat/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import chatRoutes from './routes/chat.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use("/api/v1", chatRoutes);
+
+const PORT = process.env.PORT || 5002;
+
+app.listen(PORT, () => {
+    console.log((`Chat Service Server is running on PORT ${PORT} `));
+})
